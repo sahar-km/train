07f68d4d4f7b3b181a942a80b6ae25b138ab2f53
Strawberry <165325276+sahar-km@users.noreply.github.com>
2025-10-31 21:34:35 +0330

    Add WORDS constant with an array of strings

---
+++ constants.ts
@@ -0,0 +1,7 @@
+
+export const WORDS: string[] = [
+    'ubiquitous', 'ephemeral', 'sycophant', 'idiosyncratic', 'magnanimous',
+    'pulchritudinous', 'eloquent', 'benevolent', 'serendipity', 'resilient',
+    'pragmatic', 'gregarious', 'ambiguous', 'quixotic', 'cacophony',
+    'efficacious', 'mellifluous', 'perspicacious', 'insidious', 'obfuscate'
+];
